7720bd42808c3300bb849f13a09638cce5004e4b
Maria Arteaga <maryughx@gmail.com>
2023-06-18 11:05:25 -0300

    Haciendo TDA masas

--- masa.h
+++ masa.h
@@ -1,17 +1,31 @@
-//TDA masa:
+#ifndef MASA_H
+#define MASA_H
 
+//TDA que representa una masa.
+//Contiene la posición inicial de la masa y su valor.
 typedef struct masa{
-    int xi, yi; //Posicion en la que se crea la masa
-    float masa; //Valor de la masa, sera igual para todas las masas creadas
+    int xi, yi;
+    float masa;
 } masa_t;
 
-masa_t *masa_crear();//Crea la masa, Pos: devuelve NULL si no se pudo crear o un puntero a la masa creada
-void masa_destruir(masa_t *m);//Destruye la masa. Pre:La masa fue creada
-void obtener_posicion(const masa_t *m, int *pos_x, int *pos_y); //devuelve por interfaz la posicion de la masa, 
-//Pre: La masa no es NULL, Pos: Se devuelve la posicion de la masa sin modificarla
+//Crea una masa.
+//Post: En caso de no poder crear la masa devuelve NULL.
+masa_t *masa_crear();
 
-bool hay_masa(int x, int y) // Para las posiciones introducidas, devuelve si hay una masa en ese lugar o no.
+//Destruye la masa.
+//Pre: La masa debe estar previamente creada.
+void masa_destruir(masa_t *m);
+
+//Devuelve la posición de una masa.
+//Pre: Masa != NULL.
+//Post: Devuelve la posicion de la masa sin modificarla.
+void obtener_posicion(const masa_t *m, int *pos_x, int *pos_y);
+
+//Devuelve true en caso de existir una masa en la posición querida.
+bool hay_masa(int x, int y);
 //Itero todas las masas que tengo en la simulacion y si consigo una en esa posicion devuelvo true.
 
-void cambiar_posicion_masa(masa_t *masa) //Recibe un puntero a masa y cambia su posicion.
+void cambiar_posicion_masa(masa_t *masa); //Recibe un puntero a masa y cambia su posicion.
 //Pre: La masa no es NULL. 
+
+#endif
