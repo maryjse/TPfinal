bff1c2d76516ac864a955d6e6cb03912a42c4951
Leonardo Hernandez <leojosehhh@gmail.com>
2023-06-19 15:06:12 -0300

    Cambios en masa.c

--- masa.c
+++ masa.c
@@ -1,5 +1,5 @@
 #include "masa.h"
-
+#include <stdlib.h>
 masa_t *masa_crear(size_t xi, size_t yi) {
     masa_t *m = malloc(sizeof(masa_t));
     if(m == NULL)
@@ -12,7 +12,7 @@ masa_t *masa_crear(size_t xi, size_t yi) {
 }
 
 void masa_destruir(masa_t *m) {
-    free(m);
+    free(m);   
 }
 
 void obtener_posicion(const masa_t *m, size_t *pos_x, size_t *pos_y){
@@ -21,4 +21,9 @@ void obtener_posicion(const masa_t *m, size_t *pos_x, size_t *pos_y){
 }
 
 bool hay_masa(size_t x, size_t y){ //Esta funcion creo que es mejor desarrollarla en la malla con la lista de masas
+}
+
+void cambiar_posicion_masa(masa_t *masa, size_t nueva_x, size_t nueva_y){
+    masa -> xi = nueva_x;
+    masa -> yi = nueva_y;
 }
