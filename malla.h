36d679897cb940b5a728a916dff30983f5c7f1de
Leonardo Hernandez <leojosehhh@gmail.com>
2023-06-21 16:03:04 -0300

    primitivas de la malla

--- malla.h
+++ malla.h
@@ -1,10 +1,12 @@
+#ifndef MALLA_H
+#define MALLA_H
+
 #include "lista.h"
+#include "masa.h"
+#include "resorte.h"
 
-//TDA malla:
-//La malla debe relacionar las masas con los resortes.
-
-typedef struct masas {
-    masa_t *masas;
+typedef struct masas{
+    masa_t *masa[MASA_TOTAL];
     size_t masas_totales;
 } masas_t;
 
@@ -13,20 +15,45 @@ typedef struct resortes {
     size_t resortes_totales;
 } resortes_t;
 
-typedef struct malla{
-masa_t *masa; //Puntero que apunta a la direccion de memoria apuntada por cada una de las masas almacenadas;
+//TDA que representa una malla.
+//Contiene las masas y los resortes creados.
+typedef struct malla malla_t;
+
+//Destruye la malla.
+//Pre: la masa y los resortes deben existir previamente.
+//Post: destruye todas las masas y los resortes.
+void malla_destruir(malla_t *malla);
+
+//Mueve un nodo de la malla.
+void mover_nodo(malla_t *malla);
+
+//Agrega una masa a la malla.
+//Pre: Masa != NULL.
+//Post: Si el agregado fue existoso devuelve true.
+bool agregar_masa();
+
+//Agrega un resorte a la malla.
+//Pre: Resorte != NULL.
+//Post: Si el agregado fue existoso devuelve true.
+bool agregar_resorte();
+
+//Verifica si hay alguna masa o resorte en la malla.
+//Post: Si esta vacia devuelve true.
+bool malla_esta_vacia();
+
+//Verfica si hay una masa en la posicion indicada.
+bool hay_masa();
+
+//Verfica si hay un resorte en la posicion indicada.
+bool hay_resorte();
+
+//NOTAS:
+
+//Puntero que apunta a la direccion de memoria apuntada por cada una de las masas almacenadas;
 //Si se crea una masa, se hace un realloc en este puntero que agregue una nueva masa en fase de construccion.
 //Inicialmente se crea con las dos masas fijas, por lo tanto:
- masas = malloc(2*sizeof(masa_t*));
-*masas[i] = malloc(sizeof(masa_t)); 0 <= i < 2; //(para el inicio)
-
-lista_t *resorte; // Resortes que estan dentro de la malla, linkeados a las masas a traves de los punteros
+// masas = malloc(2*sizeof(masa_t*));
+// *masas[i] = malloc(sizeof(masa_t));
+// Resortes que estan dentro de la malla, linkeados a las masas a traves de los punteros
 //a la masa en el momento de su creacion.
-} malla_t;
-
-malla_t *malla_crear();
-void malla_destruir(malla_t *malla);
-void cambiar_posicion_malla(malla_t *malla);
-void agregar_masa();
-void agregar_resorte();
-bool malla_esta_vacia();
+#endif
